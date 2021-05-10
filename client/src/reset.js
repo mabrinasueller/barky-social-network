e5decb9e3207085aebe45b82b54719556d707c68
Sabrina <schprini@yahoo.de>
2021-05-10 22:57:27 +0200

    adds reset-password component and logic

--- client/src/reset.js
+++ client/src/reset.js
@@ -10,20 +10,44 @@ export default class Reset extends Component {
         };
     }
     submit() {
-        axios
-            .post("/password-reset", {
-                email: this.state.email,
-            })
-            .then(({ data }) => {
-                if (data.success) {
-                    location.replace("/");
-                } else {
-                    console.log("error");
-                    this.setState({
-                        error: true,
-                    });
-                }
-            });
+        this.setState({
+            error: null,
+        });
+        if (this.state.view === 1) {
+            axios
+                .post("/password/reset/start", {
+                    email: this.state.email,
+                })
+                .then(() => {
+                    this.setState = {
+                        view: 2,
+                    };
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.setState = {
+                        error: "An error occcured, please try again.",
+                    };
+                });
+        } else if (this.state.view === 2) {
+            axios
+                .post("/password/reset/verify", {
+                    email: this.state.email,
+                    code: this.state.code,
+                    password: this.state.password,
+                })
+                .then(() => {
+                    this.setState = {
+                        view: 3,
+                    };
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.setState = {
+                        error: "An error occcured, please try again.",
+                    };
+                });
+        }
     }
     handleChange({ target }) {
         console.log(target.value);
@@ -31,32 +55,78 @@ export default class Reset extends Component {
             [target.name]: target.value,
         });
     }
-    render() {
-        return (
-            <div>
-                {this.state.error && (
-                    <div className="error">Email does not exist</div>
-                )}
-                <form>
-                    <div className="form__group">
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className="form__input"
-                            name="password"
-                            onChange={(e) => this.handleChange(e)}
-                        />
-                    </div>
+    determineViewToRender() {
+        if (this.state.view === 1) {
+            return (
+                <div>
+                    <form>
+                        <div className="form__group">
+                            <input
+                                type="email"
+                                placeholder="Your Email"
+                                className="form__input"
+                                name="email"
+                                required
+                                onChange={(e) => this.handleChange(e)}
+                            />
+                        </div>
 
-                    <button
-                        className="btn"
-                        type="button"
-                        onClick={() => this.submit()}
-                    >
-                        Submit
-                    </button>
-                </form>
-            </div>
-        );
+                        <button
+                            className="btn"
+                            type="button"
+                            onClick={() => this.submit()}
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            );
+        } else if (this.state.view === 2) {
+            return (
+                <div>
+                    <form>
+                        <div className="form__group">
+                            <input
+                                type="text"
+                                placeholder="Verification Code"
+                                className="form__input"
+                                name="code"
+                                required
+                                onChange={(e) => this.handleChange(e)}
+                            />
+                        </div>
+                        <div className="form__group">
+                            <input
+                                type="password"
+                                placeholder="Enter new Password"
+                                className="form__input"
+                                name="password"
+                                required
+                                onChange={(e) => this.handleChange(e)}
+                            />
+                        </div>
+
+                        <button
+                            className="btn"
+                            type="button"
+                            onClick={() => this.submit()}
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            );
+        } else if (this.state.view === 3) {
+            return (
+                <div>
+                    <h2>Password has been successfully updated!</h2>
+                    <Link to="/login">Log into your profile</Link>;
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return this.determineViewToRender();
     }
 }
