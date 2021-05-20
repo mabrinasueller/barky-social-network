bb2ac3d3829c95c8281e7b1b0896875396c2d1f2
Sabrina <schprini@yahoo.de>
2021-05-20 10:19:42 +0200

    adds styling

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -6,25 +6,27 @@ export default function Profile(props) {
     console.log("Props in Profile: ", props);
     return (
         <div className="content">
-            <div className="profile-container">
-                <div className="profile-picture-container">
-                    <div>
-                        <img
-                            alt={`${props.firstName} ${props.lastName}`}
-                            src={props.imgUrl}
-                            onClick={props.toggleUploader}
-                            className="h-21 rounded-xl "
-                        />
+            <div className="profile">
+                <div className="profile-top">
+                    <div className="profile-picture">
+                        <div>
+                            <img
+                                alt={`${props.firstName} ${props.lastName}`}
+                                src={props.imgUrl}
+                                onClick={props.toggleUploader}
+                                className="h-21 rounded-xl "
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div className="user-info-container">
-                    <h2>
-                        {props.firstName} {props.lastName}
-                    </h2>
-                    <h3>{props.bio}</h3>
-                    <BioEditor bio={props.bio} setBio={props.setBio} />
-                    <FriendButton />
+                    <div className="user-info-container">
+                        <h2>
+                            {props.firstName} {props.lastName}
+                        </h2>
+                        <h3>{props.bio}</h3>
+                        <BioEditor bio={props.bio} setBio={props.setBio} />
+                        <FriendButton />
+                    </div>
                 </div>
             </div>
         </div>
